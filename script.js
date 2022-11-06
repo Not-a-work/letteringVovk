2603b9561a8977def474a5e9aa92ed6b2114df39
Dmitriy <ivanov.dmwork@yandex.ru>
2022-11-06 16:12:58 +0300

    start

---
+++ script.js
@@ -0,0 +1,26 @@
+const App = {
+    data() {
+      return {
+        img: [
+            {item: ('/img/1.jpg')},
+            {item: ('/img/2.jpg')},
+            {item: ('/img/3.jpg')},
+            {item: ('/img/4.jpg')},
+            {item: ('/img/5.jpg')},
+            {item: ('/img/6.jpg')},
+            {item: ('/img/7.jpg')},
+            {item: ('/img/8.jpg')},
+            {item: ('/img/9.jpg')},
+            {item: ('/img/10.jpg')},
+            {item: ('/img/11.jpg')},
+            {item: ('/img/12.jpg')},
+            {item: ('/img/13.jpg')},
+            {item: ('/img/14.jpg')},
+            {item: ('/img/15.jpg')},
+        ]
+      }
+    }
+  }
+  
+  Vue.createApp(App).mount('#app')
+  
